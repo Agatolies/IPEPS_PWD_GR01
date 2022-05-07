12f0d9a0c0b85b013b4977388b0ff024f6a9b7e3
Agatolies <42890726+Agatolies@users.noreply.github.com>
2022-05-07 21:11:27 +0200

    modification wallet

--- backend/src/main/java/ipeps/pwd/wallet/payload/updatePayload/WalletUpdatePayload.java
+++ backend/src/main/java/ipeps/pwd/wallet/payload/updatePayload/WalletUpdatePayload.java
@@ -1,11 +1,20 @@
 package ipeps.pwd.wallet.payload.updatePayload;
 
+import ipeps.pwd.wallet.entity.Transaction;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class WalletUpdatePayload {
+    private int wallet_id;
+    private String name;
+    private String description;
+    private boolean actif;
+    private String type;
+    private List<Transaction> transactions;
 }
