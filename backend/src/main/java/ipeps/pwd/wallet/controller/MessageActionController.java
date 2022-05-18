1d0a2e70fbc55635b34c3f738402c04e546a2b19
AnneFifi <101360423+AnneFifi@users.noreply.github.com>
2022-05-18 18:55:43 +0200

    création du controller MessageActionController

--- backend/src/main/java/ipeps/pwd/wallet/controller/MessageActionController.java
+++ backend/src/main/java/ipeps/pwd/wallet/controller/MessageActionController.java
@@ -1,17 +1,32 @@
 package ipeps.pwd.wallet.controller;
 
-import ipeps.pwd.wallet.builder.EmployeeBuilder;
 import ipeps.pwd.wallet.common.entity.response.ApiResponse;
-import ipeps.pwd.wallet.entity.Employee;
-import ipeps.pwd.wallet.payload.createPayload.EmployeeCreatePayload;
-import ipeps.pwd.wallet.payload.updatePayload.EmployeeUpdatePayload;
-import ipeps.pwd.wallet.repository.MessageActionRepository;
+import ipeps.pwd.wallet.payload.createPayload.MessageActionCreatePayload;
+import ipeps.pwd.wallet.payload.updatePayload.MessageActionUpdatePayload;
+import ipeps.pwd.wallet.service.MessageActionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("messageAction")
 public class MessageActionController {
     @Autowired
-    MessageActionRepository messageActionRepository;
+    MessageActionService messageActionService;
+
+    @GetMapping("list")
+    public ApiResponse list(){ return messageActionService.list(); }
+
+    @GetMapping("detail/{id}")
+    public ApiResponse detail(@PathVariable("id") int id){ return messageActionService.detail(id); }
+
+    @PutMapping("update")
+    public ApiResponse update(@RequestBody MessageActionUpdatePayload payload){ return messageActionService.update(payload); }
+
+    @PostMapping("create")
+    public ApiResponse create(@RequestBody MessageActionCreatePayload payload){ return messageActionService.create(payload); }
+
+    @DeleteMapping("delete/{id}")
+    public ApiResponse delete(@PathVariable("id") int id){ return messageActionService.delete(id); }
 
 
 }
