c317686988d8bf8d321b51204516a3f4191afa96
Blackfist696 <57995425+Blackfist696@users.noreply.github.com>
2022-06-01 20:43:08 +0200

    Insertion "organization" et son organization-router

---
+++ frontend/src/app/modules/organization/component/organization-router/organization-router.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-router',
+  templateUrl: './organization-router.component.html',
+  styleUrls: ['./organization-router.component.scss']
+})
+export class OrganizationRouterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
