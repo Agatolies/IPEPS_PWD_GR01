ed0ebddbe7e93bb6bfea2c0e39338f2820bb11a3
Blackfist696 <57995425+Blackfist696@users.noreply.github.com>
2022-06-01 20:43:08 +0200

    Insertion "organization" et son organization-router

--- frontend/src/app/modules/organization/organization.module.ts
+++ frontend/src/app/modules/organization/organization.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrganizationRoutingModule } from './organization-routing.module';
+import { OrganizationRouterComponent } from './component/organization-router/organization-router.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OrganizationRouterComponent
+  ],
   imports: [
     CommonModule,
     OrganizationRoutingModule
