6966ddaefe5528a2517c69d104672325d6891223
Blackfist696 <57995425+Blackfist696@users.noreply.github.com>
2022-06-01 20:43:08 +0200

    Insertion "organization" et son organization-router

--- frontend/src/app/modules/organization/organization-routing.module.ts
+++ frontend/src/app/modules/organization/organization-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PublicGuard} from "@security/guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'organization',
+    loadChildren: () => import('./component/organization-router/organization-router.component').then(m => m.OrganizationRouterComponent)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
