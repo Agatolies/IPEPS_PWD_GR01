055c56f22bd222eac8a000783a6f58d639c57cc3
Andrea-Sochiko <andreahouben25@gmail.com>
2022-06-05 18:53:10 +0200

    Modules contact création squelette

--- frontend/src/app/modules/contact/contact.module.ts
+++ frontend/src/app/modules/contact/contact.module.ts
@@ -4,12 +4,24 @@ import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactFormComponent } from './component/contact-form/contact-form.component';
 import { ContactMenuComponent } from './component/contact-menu/contact-menu.component';
+import { ContactCreateComponent } from './page/ecosystem/contact-create/contact-create.component';
+import { ContactDeleteComponent } from './page/ecosystem/contact-delete/contact-delete.component';
+import { ContactUpdateComponent } from './page/ecosystem/contact-update/contact-update.component';
+import { ContactHomeComponent } from './page/ecosystem/contact-home/contact-home.component';
+import { ContactRouteurComponent } from './page/ecosystem/contact-routeur/contact-routeur.component';
+import { ContactDetailComponent } from './page/ecosystem/contact-detail/contact-detail.component';
 
 
 @NgModule({
   declarations: [
     ContactFormComponent,
-    ContactMenuComponent
+    ContactMenuComponent,
+    ContactCreateComponent,
+    ContactDeleteComponent,
+    ContactUpdateComponent,
+    ContactHomeComponent,
+    ContactRouteurComponent,
+    ContactDetailComponent
   ],
   imports: [
     CommonModule,
---
+++ frontend/src/app/modules/contact/page/ecosystem/contact-delete/contact-delete.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ContactDeleteComponent } from './contact-delete.component';
+
+describe('ContactDeleteComponent', () => {
+  let component: ContactDeleteComponent;
+  let fixture: ComponentFixture<ContactDeleteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ContactDeleteComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContactDeleteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
