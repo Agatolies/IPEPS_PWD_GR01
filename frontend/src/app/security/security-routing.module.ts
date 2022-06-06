f20c8ceb2235791440e8c1ec0b215abf867a02bd
Agatolies <laure.deste@gmail.com>
2022-06-06 19:43:53 +0200

    Update security-routing.module.ts

--- frontend/src/app/security/security-routing.module.ts
+++ frontend/src/app/security/security-routing.module.ts
@@ -1,18 +1,25 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {SigninComponent, SignupComponent} from '@security/component';
+import {DashboardComponent} from "@dashboard/component";
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'signin',
-    pathMatch: 'full',
-  }, {
-    path: 'signin',
-    component: SigninComponent
-  }, {
-    path: 'signup',
-    component: SignupComponent
+    component: DashboardComponent, // Launch all children roads in the router outlet of this component (DashboardComponent)
+    children: [
+      {
+        path: '',
+        redirectTo: 'signin',
+        pathMatch: 'full',
+      }, {
+        path: 'signin',
+        component: SigninComponent
+      }, {
+        path: 'signup',
+        component: SignupComponent
+      }
+    ]
   }
 ];
 
