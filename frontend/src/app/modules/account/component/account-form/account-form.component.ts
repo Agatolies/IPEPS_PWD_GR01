e61591cdcc310c7dc646c127f1054f6fb260dff0
Blackfist696 <57995425+Blackfist696@users.noreply.github.com>
2022-06-07 02:12:15 +0200

    Pascal accountform module et html (à corriger)

--- frontend/src/app/modules/account/component/account-form/account-form.component.ts
+++ frontend/src/app/modules/account/component/account-form/account-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiResponse} from "@shared/model";
+import {AccountCreatePayload} from "@account/model";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AccountService} from "@account/service/account.service";
 
 @Component({
   selector: 'app-account-form',
@@ -6,8 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-form.component.scss']
 })
 export class AccountFormComponent implements OnInit {
+  response?: ApiResponse;
+  payload: AccountCreatePayload = {
+    firstname: '',
+    lastname: ''
+  }
+  title = 'accountFormGroup';
+  // new FormControl(LA_VALEUR_PAR_DEFAUT, [TABLEAU DE VALIDATION])
+  payloadFormGroup = new FormGroup({
+    firstname: new FormControl(this.payload.firstname, [Validators.required]),
+    lastname: new FormControl(this.payload.lastname, [Validators.required])
+  })
 
-  constructor() { }
+  constructor(public accountService: AccountService) {}
+
+  save(e: any): void {
+    e.preventDefault();
+    if (!this.payloadFormGroup.invalid) {
+      // ceci remplace les deux lignes commentées
+      // ... => copie les propriétés ET leur valeur dans le nouvel objet
+      // du coup ce qu'il se passe:
+      // 1. [...this.payload]  on recopie payload dans payload
+      // 2. [...this.payloadFormGroup.value] ...crée ou remplace les propriétés du second objet
+
+      this.payload = {...this.payload, ...this.payloadFormGroup.value};
+
+      // this.payload.firstname = this.payloadFormGroup.value['firstname'];
+      // this.payload.lastname = this.payloadFormGroup.value['lastname'];
+      this.accountService.create(this.payload)
+        .subscribe((response: ApiResponse) => {
+          this.response = response;
+        })
+    } else {
+      alert('Champs rempli de façon incorrecte.');
+    }
+  }
 
   ngOnInit(): void {
   }
