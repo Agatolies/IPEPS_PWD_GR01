341dba05c96c4276a3fb26b16bdf251b74df0123
Blackfist696 <57995425+Blackfist696@users.noreply.github.com>
2022-06-09 00:43:15 +0200

    Pascal corrections route signin

--- frontend/src/app/security/security-routing.module.ts
+++ frontend/src/app/security/security-routing.module.ts
@@ -1,13 +1,8 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {SigninComponent, SignupComponent} from '@security/component';
-import {DashboardComponent} from "@dashboard/component";
 
 const routes: Routes = [
-  {
-    path: '',
-    component: DashboardComponent, // Launch all children roads in the router outlet of this component (DashboardComponent)
-    children: [
       {
         path: '',
         redirectTo: 'signin',
@@ -19,8 +14,6 @@ const routes: Routes = [
         path: 'signup',
         component: SignupComponent
       }
-    ]
-  }
 ];
 
 @NgModule({
