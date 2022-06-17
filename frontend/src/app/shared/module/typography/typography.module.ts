bf7e430cd1889b37d2f893119289979356383cfc
Agatolies <laure.deste@gmail.com>
2022-06-17 20:57:29 +0200

    Ajout du nombre de Wallets

--- frontend/src/app/shared/module/typography/typography.module.ts
+++ frontend/src/app/shared/module/typography/typography.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from "@ngx-translate/core";
 import { PageTitleComponent } from '@shared/module/typography/component';
 import {MatCardModule} from "@angular/material/card";
+import {MatBadgeModule} from "@angular/material/badge";
 
 @NgModule({
   declarations: [
     PageTitleComponent
   ],
-  imports: [
-    CommonModule,
-    TranslateModule,
-    MatCardModule
-  ],
+    imports: [
+        CommonModule,
+        TranslateModule,
+        MatCardModule,
+        MatBadgeModule
+    ],
   exports: [
     PageTitleComponent
   ]
