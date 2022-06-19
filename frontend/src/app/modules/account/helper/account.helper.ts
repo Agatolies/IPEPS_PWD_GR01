2ce30ac3a95747ab7aedd2dd946bb256df2aeb38
Andrea-Sochiko <andreahouben25@gmail.com>
2022-06-19 21:39:33 +0200

    Erreur Correction

--- frontend/src/app/modules/account/helper/account.helper.ts
+++ frontend/src/app/modules/account/helper/account.helper.ts
@@ -1,29 +1,33 @@
 import {AccountCreatePayload, AccountDto, AccountUpdatePayload, Account} from "@account/model";
+import {Employee, EmployeeDto} from "@employee/model";
 
 
 export class AccountHelper {
-  public static fromDto(dto: AccountDto): Account {
+  public static fromDto(dto: AccountDto): { firstname: string; id: string; employees: EmployeeDto[]; lastname: string } {
     return {
-
       id: dto.account_id,
       firstname: dto.firstname,
-      lastname: dto.lastname
+      lastname: dto.lastname,
+      employees: dto.employees,
     }
   }
 
-  public static toDto(business: Account): AccountDto {
+  public static toDto(business: Account): { firstname: string; account_id: string; employees: Employee; lastname: string } {
     return {
       account_id: business.id,
       firstname: business.firstname,
-      lastname: business.lastname
+      lastname: business.lastname,
+      employees: business.employees
     }
   }
 
-  public static getEmpty(): Account {
+  public static getEmpty(): { firstname: string; id: string; employees: string; lastname: string } {
     return {
+
       id: '',
       firstname: '',
-      lastname: ''
+      lastname: '',
+      employees: ''
     }
   }
 
--- frontend/src/app/modules/document/compoment/document-menu/document-menu.component.ts
+++ frontend/src/app/modules/document/compoment/document-menu/document-menu.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {DataListConfig} from "@shared/module/data-list/component/data-list/data-list.config";
 import {MenuAction} from "@shared/model";
 import {MenuHelperUtils} from "@shared/helper";
 import {NavigationService} from "@shared/service";
