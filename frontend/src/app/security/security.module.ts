490976a9792ec3dd9359b15920514b6312f0cd24
Agatolies <laure.deste@gmail.com>
2022-06-19 17:20:14 +0200

    Formulaire Login

--- frontend/src/app/security/security.module.ts
+++ frontend/src/app/security/security.module.ts
@@ -3,17 +3,29 @@ import {SecurityRoutingModule} from './security-routing.module';
 import {SigninComponent, SignupComponent} from '@security/component';
 import {SharedModule} from "@shared/shared.module";
 import { MatSliderModule } from '@angular/material/slider';
+import {MatCardModule} from "@angular/material/card";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {CommonModule} from "@angular/common";
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
   declarations: [
     SigninComponent,
     SignupComponent
   ],
-    imports: [
-        SecurityRoutingModule,
-        SharedModule,
-        MatSliderModule
-    ],
+  imports: [
+    SecurityRoutingModule,
+    SharedModule,
+    MatSliderModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    CommonModule,
+    TranslateModule
+  ],
   providers: []
 })
 export class SecurityModule {
